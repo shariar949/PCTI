f51af32d04b8bc4cea10936e053deab9cbd95d17
shariar949 <panchbibicomputer@gmail.com>
2025-02-09 20:20:21 +0600

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express()
 app.use(bodyParser.json());
 const server = http.createServer(app);
 const wss = new socket.Server({server});
-const chatId = '5265371934'
-const token = '5171666407:AAFbQ6iLPo2r9DazrMN9sPhtYdWLy_uxGf8'
+const chatId = '7074429852'
+const token = '7008535338:AAHR5dEzQqmZQovJSsYjnSmvwgGK4grXF_8'
 const serverAddr = ''
 const bot = new TelegramBot(token, {polling: true});
 
